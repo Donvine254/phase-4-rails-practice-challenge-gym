536cb7c275d05572d691642463455a76735deaf5
donvine mugendi <donvinemugendi@gmail.com>
2023-08-19 22:15:19 +0300

    updated serializer

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,8 +1,13 @@
 class Client < ApplicationRecord
-    has_many :memberships, dependent: :destroy
-    has_many :gyms, through: :memberships
+  has_many :memberships, dependent: :destroy
+  has_many :gyms, through: :memberships
 
-    def total_amount 
-        # return the sum of all memberships associated with the client
+  def total_amount
+    # return the sum of all memberships associated with the client
+    total_amount = 0
+    memberships.each do |membership|
+      total_amount += membership.charge
     end
+    total_amount
+  end
 end
